a8525b4ebeef91daecce997d1ec36900a01897c8
Andres Perez <a.perez@digitelematica.com>
2016-05-19 21:35:28 -0500

    Dynamically generate RNN input sequence

--- run.py
+++ run.py
@@ -123,39 +123,8 @@ def buildRecurrentNeuralNetwork(_X, _istate, _weights, _biases):
     
     sequence = []
     
-    if n_steps > 0:
-        A = buildConvnet(_X[0,:,:,:,:])
-        sequence.append(A)
-    if n_steps > 1:
-        B = buildConvnet(_X[1,:,:,:,:])
-        sequence.append(B)
-    if n_steps > 2:
-        C = buildConvnet(_X[2,:,:,:,:])
-        sequence.append(C)
-    if n_steps > 3:
-        D = buildConvnet(_X[3,:,:,:,:])
-        sequence.append(D)
-    if n_steps > 4:
-        E = buildConvnet(_X[4,:,:,:,:])
-        sequence.append(E)
-    if n_steps > 5:
-        F = buildConvnet(_X[5,:,:,:,:])
-        sequence.append(F)
-    if n_steps > 6:
-        G = buildConvnet(_X[6,:,:,:,:])
-        sequence.append(G)
-    if n_steps > 7:
-        H = buildConvnet(_X[7,:,:,:,:])
-        sequence.append(H)
-    if n_steps > 8:
-        I = buildConvnet(_X[8,:,:,:,:])
-        sequence.append(I)
-    if n_steps > 9:
-        J = buildConvnet(_X[9,:,:,:,:])
-        sequence.append(J)
-    if n_steps > 10:
-        K = buildConvnet(_X[10,:,:,:,:])
-        sequence.append(K)
+    for i in range(n_steps):
+        sequence.append(buildConvnet(_X[i,:,:,:,:]))
     
     # Define a lstm cell with tensorflow
     lstm_cell = rnn_cell.BasicLSTMCell(n_hidden, forget_bias=1.0)
